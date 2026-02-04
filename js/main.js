807bffce2121e452bd39d17ae2875f3239a31fe9
SamarthSahabMathur <150432007+SamarthSahabMathur@users.noreply.github.com>
2026-02-04 19:49:18 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("show");
+  });
+});
 
